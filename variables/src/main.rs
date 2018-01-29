908bc59122cc33e6b80ff6465606d94620fbeb1b
Kentaro SUZUKI <kentarock@gmail.com>
2018-01-29 23:44:07 +0900

    Shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,11 +1,6 @@
 fn main() {
-    let mut x = 5;
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
     println!("The value of x is {}", x);
-
-    x = 6;
-    println!("The value of x is {}", x);
-
-    const MAX_POINTS: u32 = 100_000;
-    println!("The value of MAX_POINTS is {}", MAX_POINTS);
-
 }
