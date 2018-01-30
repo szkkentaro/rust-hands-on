6d370150242776496a8d7d444d2c09eb41276970
Kentaro SUZUKI <kentarock@gmail.com>
2018-01-30 09:04:44 +0900

    floating point with type annotation

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -2,4 +2,10 @@ fn main() {
     // type annotation
     let guess: u32 = "42".parse().expect("Not a number");
     println!("{}", guess);
+
+    // floating point
+    let x = 2.0;
+    let y: f32 = 3.0;
+    println!("{}, {}", x, y); 
+
 }
