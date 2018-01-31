c6232ef811280099e7f93ea02252d5ec557e535b
Kentaro SUZUKI <kentarock@gmail.com>
2018-02-01 06:26:42 +0900

    function pointer

--- functions_work/src/main.rs
+++ functions_work/src/main.rs
@@ -12,13 +12,20 @@ fn main() {
     let x = plus_one(5);
     println!("{}", x);
 
-    diverges()
+    //diverges()
+
+    // function pointer
+
+    //let f: fn(i32) -> i32 = plus_one;
+    let f = plus_one; // with type inference
+    println!("{}", f(9));
 }
 
 fn plus_one(x: i32) -> i32 {
     x + 1
 }
 
-fn diverges() -> ! {
-    panic!("This function never returns!");
-}
+// fn diverges() -> ! {
+//     panic!("This function never returns!");
+// }
+
