e7583ff9afb3bd2cfe16c5bfe18939043f076529
Kentaro SUZUKI <kentarock@gmail.com>
2018-02-01 08:58:58 +0900

    enumerate and loop label

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -61,4 +61,19 @@ fn main() {
         println!("n is {}", n);
     }
     println!("LIFTOFF!");
+
+    // enumerate
+    for (i, v) in (5..10).enumerate() {
+        println!("The index is {}, the value is {}", i, v);
+    }
+
+    // loop label
+    'outer: for x in 0..10 {
+        'inner: for y in 0..10 {
+            if x % 2 == 0 { continue 'outer; }
+            if y % 2 == 0 { continue 'inner; }
+            println!("{}, {}", x, y);
+        }
+    }
 }
+
