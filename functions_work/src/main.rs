83615b0e21daaa0bd889de78c06b765d37f4e10f
Kentaro SUZUKI <kentarock@gmail.com>
2018-02-01 06:19:35 +0900

    diverges

--- functions_work/src/main.rs
+++ functions_work/src/main.rs
@@ -11,8 +11,14 @@ fn main() {
 
     let x = plus_one(5);
     println!("{}", x);
+
+    diverges()
 }
 
 fn plus_one(x: i32) -> i32 {
     x + 1
+}
+
+fn diverges() -> ! {
+    panic!("This function never returns!");
 }
