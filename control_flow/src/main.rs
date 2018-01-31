f02ac6640ae74858c3a77e0e6c2e2f0b3dafba95
Kentaro SUZUKI <kentarock@gmail.com>
2018-02-01 06:35:30 +0900

    Add conttole_flow

---
+++ control_flow/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let number = 3;
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+
+    if number != 0 {
+        println!("number was something other than 0");
+    }
+}
