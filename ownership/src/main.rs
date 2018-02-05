8bd1f039640110221e8d8bffcde413807b1760ae
Kentaro SUZUKI <kentarock@gmail.com>
2018-02-06 08:51:29 +0900

    Add return values and scope

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -10,6 +10,17 @@ fn main() {
                                    // but, i32 is Copy, so it's okay to still
                                    // use x afterward.
 
+    // return values and scope
+    let s1 = gives_ownership();
+    let s2 = String::from("hello");
+    let s3 = takes_and_gives_back(s2);
+    println!("{}, {}", s1, s3);
+
+    // return multiple values
+    let s1 = String::from("hello");
+    let (s2, len) = calcurate_length(s1);
+    println!("The length of '{}' is {}.", s2, len);
+
     // primitive value has Copy type
     let a = 5;
     let _y = double(a);
@@ -40,6 +51,20 @@ fn takes_ownership(some_string: String) {
 
 fn makes_copy(some_integer: i32) {
     println!("{}", some_integer);
+}
+
+fn gives_ownership() -> String {
+    let some_string = String::from("hello");
+    some_string
+}
+
+fn takes_and_gives_back(a_string: String) -> String {
+    a_string
+}
+
+fn calcurate_length(s: String) -> (String, usize) {
+    let length = s.len();
+    (s, length)
 }
 
 fn foo(v1: &Vec<i32>, v2: &Vec<i32>) -> i32 {
