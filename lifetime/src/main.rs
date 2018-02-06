f5931c988eaba174d351aeed2f70f672b63c3d6d
Kentaro SUZUKI <kentarock@gmail.com>
2018-02-07 08:45:41 +0900

    impl

--- lifetime/src/main.rs
+++ lifetime/src/main.rs
@@ -2,8 +2,13 @@ struct Foo<'a> {
     x: &'a i32,
 }
 
+impl<'a> Foo<'a> {
+    fn x(&self) -> &'a i32 { self.x }
+}
+
 fn main() {
     let y = &5; // same as let _y = 5; let y = &_y;
     let f = Foo{x: y};
-    println!("{}", f.x);
+
+    println!("{}", f.x());
 }
