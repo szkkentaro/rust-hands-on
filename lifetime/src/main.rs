d5b42f61f41637cc07b175c0e881c9ed91a75c21
Kentaro SUZUKI <kentarock@gmail.com>
2018-02-07 08:41:57 +0900

    struct

--- lifetime/src/main.rs
+++ lifetime/src/main.rs
@@ -1,3 +1,9 @@
+struct Foo<'a> {
+    x: &'a i32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let y = &5; // same as let _y = 5; let y = &_y;
+    let f = Foo{x: y};
+    println!("{}", f.x);
 }
