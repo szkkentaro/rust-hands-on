941ccfc358b4b91f395879e93f3994156d082b89
Kentaro SUZUKI <kentarock@gmail.com>
2018-02-08 08:43:32 +0900

    feat: Add Cell sample

--- mutability/src/main.rs
+++ mutability/src/main.rs
@@ -1,6 +1,12 @@
+use std::cell::Cell;
+
 struct Point {
     x: i32,
     y: i32
+}
+struct PointWithCell {
+    x: i32,
+    y: Cell<i32>
 }
 
 fn main() {
@@ -8,5 +14,9 @@ fn main() {
     a.x = 10;
     let b = Point { x: 5, y: 6 };
     //b.x = 10; // cannot mutably borrow immutable field
-    println!("{}, {}", b.x, b.y);
+    println!("x: {}, y: {}", b.x, b.y);
+    let c = PointWithCell {x: 5, y: Cell::new(6)};
+    c.y.set(7);
+
+    println!("x: {}, y: {:?}", c.x, c.y);
 }
