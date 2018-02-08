fe22cbe09017c4f832218b742b720d48d406fcea
Kentaro SUZUKI <kentarock@gmail.com>
2018-02-09 08:52:09 +0900

    feat: Add struct filed sample

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,8 +1,15 @@
 struct Point {
     x: i32,
     y: i32
+    // z: mut i32 : can not make field mutable
 }
+
 fn main() {
-    let p = Point {x: 0, y: 0};
+    let mut p = Point {x: 0, y: 0};
+    p.x = 5;
+    
+    let p = p; // this trick make field mutable
+    p.y = 6; // can not mutably borrow immutable field
+
     println!("p is at ({}, {})", p.x, p.y);
 }
