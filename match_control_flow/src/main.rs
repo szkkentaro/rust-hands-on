0c45ad91d423e46fe302b42f24b88e6d3586e748
Kentaro SUZUKI <kentarock@gmail.com>
2018-02-10 07:16:10 +0900

    feat: Add _ placeholder sample

--- match_control_flow/src/main.rs
+++ match_control_flow/src/main.rs
@@ -6,4 +6,19 @@ fn main() {
         3 => println!("three"),
         _ => println!("something else")
     }
+    let num = match x {
+        1 => "one",
+        2 => "two",
+        3 => "three",
+        _ => "something else"
+    };
+    println!("{}", num);
+
+    let some_u8_value = 0u8;
+    match some_u8_value {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        _ => () // do nothing
+    }
 }
