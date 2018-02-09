98d7ded37b39c85aa2925c52ed03f8e4252714b4
Kentaro SUZUKI <kentarock@gmail.com>
2018-02-10 07:08:59 +0900

    feat: Add match sample

--- match_control_flow/src/main.rs
+++ match_control_flow/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 3;
+    match x {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        _ => println!("something else")
+    }
 }
