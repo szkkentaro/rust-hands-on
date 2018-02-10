1d6a3b4c5568d61367e7cf6247f7618e608e43b5
Kentaro SUZUKI <kentarock@gmail.com>
2018-02-11 05:29:43 +0900

    feat: Add an enum and a match

--- match_control_flow/src/main.rs
+++ match_control_flow/src/main.rs
@@ -21,4 +21,25 @@ fn main() {
         3 => println!("three"),
         _ => () // do nothing
     }
+
+    // an enum and a match
+    enum Coin {
+        Penny,
+        Nickel,
+        Dime,
+        Quarter,
+    }
+    fn value_in_cents(coin: Coin) -> u32 {
+        match coin {
+            Coin::Penny => 1,
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter => 25,
+        }
+    }
+    let p = Coin::Penny;
+    let n = Coin::Nickel;
+    let d = Coin::Dime;
+    let q = Coin::Quarter;
+    println!("{}, {}, {}, {}", value_in_cents(p), value_in_cents(n), value_in_cents(d), value_in_cents(q));
 }
