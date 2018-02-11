e553c6405c1d746b6ff14d003584018943a15081
Kentaro SUZUKI <kentarock@gmail.com>
2018-02-12 05:31:18 +0900

    feat: Add multiple match

--- patterns/src/main.rs
+++ patterns/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let x = "x";
+    let c = "c";
+    match c {
+        // shadowing
+        x => println!("x: {}, c: {}", x, c),
+    }
+    println!("x: {}", x);
+
+    // multiple
+    let x = 1;
+    match x {
+        1 | 2 => println!("one or two"),
+        _ => println!("anything"),
+    }
 }
