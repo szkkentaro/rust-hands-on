366debc9b560d15388144633fc4dbd085331521f
Kentaro SUZUKI <kentarock@gmail.com>
2018-02-12 05:36:55 +0900

    feat: Add destructuring sample

--- patterns/src/main.rs
+++ patterns/src/main.rs
@@ -13,4 +13,20 @@ fn main() {
         1 | 2 => println!("one or two"),
         _ => println!("anything"),
     }
+
+    struct Point {
+        x: i32,
+        y: i32,
+    }
+    let origin = Point {x: 0, y: 0};
+
+    match origin {
+        Point {x, y} => println!("{}, {}", x, y),
+    }
+    match origin {
+        Point {x: x1, y: y1} => println!("{}, {}", x1, y1),
+    }
+    match origin {
+        Point {y, .. } => println!("y is {}", y),
+    }
 }
