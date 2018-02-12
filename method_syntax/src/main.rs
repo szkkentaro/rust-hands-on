e69344fe39993ee3dd54139a901e8cffd5c71a95
Kentaro SUZUKI <kentarock@gmail.com>
2018-02-13 06:31:55 +0900

    feat: call method sample

--- method_syntax/src/main.rs
+++ method_syntax/src/main.rs
@@ -1,3 +1,42 @@
+struct Circle {
+    x: f64,
+    y: f64,
+    radius: f64
+}
+impl Circle {
+    fn area(&self) -> f64 {
+        std::f64::consts::PI * (self.radius * self.radius)
+    }
+    fn reference(&self) {
+        println!("taking self by reference!");
+    }
+    fn mutable_reference(&mut self) {
+        self.x = 1.0;
+        println!("taking self by mutable reference!");
+    }
+}
+
+impl Circle {
+    fn takes_ownership(self) {
+        println!("taking ownership of self!");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let c = Circle {
+        x: 0.0,
+        y: 0.0,
+        radius: 2.0,
+    };
+    println!("{},{},{}", c.area(), c.x, c.y);
+    c.reference();
+    c.takes_ownership();
+   
+    let mut c1 = Circle {
+        x: 0.0,
+        y: 0.0,
+        radius: 2.0,
+    };
+    c1.mutable_reference();
+    println!("{}", c1.x);
 }
