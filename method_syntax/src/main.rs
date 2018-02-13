14ad8330250a919b8f5696418fad1e374dcdae01
Kentaro SUZUKI <kentarock@gmail.com>
2018-02-14 08:48:49 +0900

    feat: Add associated function

--- method_syntax/src/main.rs
+++ method_syntax/src/main.rs
@@ -4,8 +4,14 @@ struct Circle {
     radius: f64
 }
 impl Circle {
+    fn new(x: f64, y: f64, radius: f64) -> Circle {
+        Circle { x: x, y: y, radius: radius }
+    }
     fn area(&self) -> f64 {
         std::f64::consts::PI * (self.radius * self.radius)
+    }
+    fn grow(&self, increment: f64) -> Circle {
+        Circle { x: self.x, y: self.y, radius: self.radius + increment }
     }
     fn reference(&self) {
         println!("taking self by reference!");
@@ -30,13 +36,10 @@ fn main() {
     };
     println!("{},{},{}", c.area(), c.x, c.y);
     c.reference();
+    println!("{}", c.grow(2.0).area());
     c.takes_ownership();
    
-    let mut c1 = Circle {
-        x: 0.0,
-        y: 0.0,
-        radius: 2.0,
-    };
+    let mut c1 = Circle::new(0.0, 0.0, 2.0);
     c1.mutable_reference();
     println!("{}", c1.x);
 }
