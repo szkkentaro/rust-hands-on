a52b4800b69267d323e31adc5c3577f23885719c
Kentaro SUZUKI <kentarock@gmail.com>
2018-02-15 08:51:03 +0900

    feat: Add &str , String sample

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    println!("Hello, world!");
+    let greeting = "Hello world!"; // &'static str type
+    println!("{}", greeting);
+
+    let s = "foo
+    bar";
+    assert_eq!("foo\n    bar", s);
+
+    let s = "foo\
+    bar";
+    assert_eq!("foobar", s);
+
+    let mut s = "Hello".to_string(); // mut s is String
+    println!("{}", s);
+    s.push_str(", world");
+    println!("{}", s);
+
+    fn take_slice(slice: &str) {
+        println!("{}", slice);
+    }
+    let x = "Hey";
+    take_slice(x);
+
 }
+
