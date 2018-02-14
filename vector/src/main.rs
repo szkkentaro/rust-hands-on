659b029e65b7caa6bd166081a235a36f71dee96c
Kentaro SUZUKI <kentarock@gmail.com>
2018-02-14 12:41:05 +0900

    feat: Add iterating sample

--- vector/src/main.rs
+++ vector/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let v = vec![0; 3];
+    for i in &v {
+        println!("A reference to {}", i);
+    }
+    println!("Vector index 1 is {}", v[1]);
+
+    let mut v = vec![1,2,3];
+    for i in &mut v {
+        println!("A mutable reference to {}", i);
+    }
+    for i in v {
+        println!("Take ownership of the vector and its element {}", i);
+    }
 }
