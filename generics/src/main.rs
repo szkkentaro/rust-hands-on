6006198a6caea38310e777d39deb97b126e6042e
Kentaro SUZUKI <kentarock@gmail.com>
2018-02-17 19:08:13 +0900

    feat: Add generics struct sample

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,5 +1,21 @@
 fn main() {
+
     let x: Option<i32> = Some(5);
     let y: Option<f64> = Some(5.0f64);
     println!("{:?}, {:?}", x, y);
+
+    struct Point<T> {
+        x: T,
+        y: T
+    }
+    impl<T> Point<T> {
+        fn swap(&mut self) {
+            std::mem::swap(&mut self.x, &mut self.y);
+        }
+    }
+
+    let mut point = Point { x: Some(1), y: Some(2)} ;
+    point.swap();
+
+    println!("Swaped values x:{:?}, y:{:?}", point.x, point.y);
 }
