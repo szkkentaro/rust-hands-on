6d6c189c40eaf21f7f34abd4f4cace13b086fb16
Kentaro SUZUKI <kentarock@gmail.com>
2018-02-19 09:22:16 +0900

    feat: Add trait bounds

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -1,11 +1,12 @@
+#![allow(dead_code)]
+trait HasArea {
+    fn area(&self) -> f64;
+}
+
 struct Circle {
     x: f64,
     y: f64,
     radius: f64
-}
-
-trait HasArea {
-    fn area(&self) -> f64;
 }
 
 impl HasArea for Circle {
@@ -14,9 +15,37 @@ impl HasArea for Circle {
     }
 }
 
+struct Square {
+    x: f64,
+    y: f64,
+    side: f64
+}
+
+impl HasArea for Square {
+    fn area(&self) -> f64 {
+        self.side * self.side
+    }
+}
+
+fn print_area<T: HasArea>(shape: T) {
+    println!("The shape of area is {}", shape.area());
+}
 
 fn main() {
-    let c = Circle { x: 1.0, y: 1.0, radius: 1.0};
-    println!("x: {}, y: {}", c.x, c.y);
-    println!("radius: {}, area(): {}", c.radius, c.area());
+    let c = Circle {x: 1.0, y: 1.0, radius: 1.0};
+    println!("c.area() is {}", c.area());
+
+    let circle = Circle {
+        x: 0.0,
+        y: 0.0, 
+        radius: 1.0
+    };
+    let square = Square {
+        x: 0.0,
+        y: 0.0, 
+        side: 1.0
+    };
+    print_area(circle);
+    print_area(square);
+    
 }
