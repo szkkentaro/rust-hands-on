a2b6677e8e0cefe75bf569117401eef81d317ba3
Kentaro SUZUKI <kentarock@gmail.com>
2018-02-20 08:59:31 +0900

    feat: Add generics struct with trait

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -31,6 +31,19 @@ fn print_area<T: HasArea>(shape: T) {
     println!("The shape of area is {}", shape.area());
 }
 
+// generics struct 
+struct Rectangle<T> {
+    x: T,
+    y: T,
+    width: T,
+    height: T,
+}
+impl <T: PartialEq> Rectangle<T> {
+    fn is_squea(&self) -> bool {
+        self.width == self.height
+    }
+}
+
 fn main() {
     let c = Circle {x: 1.0, y: 1.0, radius: 1.0};
     println!("c.area() is {}", c.area());
@@ -47,5 +60,18 @@ fn main() {
     };
     print_area(circle);
     print_area(square);
+
+    // generics struct
+    let mut r = Rectangle{
+        x: 0,
+        y: 0,
+        width: 47,
+        height: 47,
+    };
+    assert!(r.is_squea());
+
+    // change width
+    r.width = 42;
+    assert!(!r.is_squea());
     
 }
