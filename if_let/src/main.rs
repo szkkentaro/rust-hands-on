4a39c98308c7f2eb18b523309ddcdb8cb11445ba
Kentaro SUZUKI <kentarock@gmail.com>
2018-02-23 08:46:11 +0900

    feat: Add while let syntax sample

--- if_let/src/main.rs
+++ if_let/src/main.rs
@@ -7,4 +7,15 @@ fn main() {
     if let Some(x) = opt {
         foo(x);
     }
+
+    let mut v = vec![1,3,5,7,11];
+    // loop {
+    //     match v.pop() {
+    //         Some(x) => println!("{}", x),
+    //         None => break,
+    //     }
+    // }
+    while let Some(x) = v.pop() {
+        println!("{}", x);
+    }
 }
