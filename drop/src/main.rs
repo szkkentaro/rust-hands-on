8703aa87f5ef1558041c29abe9cc2f3b497f8dd5
Kentaro SUZUKI <kentarock@gmail.com>
2018-02-22 09:24:43 +0900

    feat: Add drop order sample

--- drop/src/main.rs
+++ drop/src/main.rs
@@ -4,6 +4,17 @@ impl Drop for HasDrop {
         println!("Dropping!");
     }
 }
+struct Firework {
+    strength: i32
+}
+impl Drop for Firework {
+    fn drop(&mut self) {
+       println!("BOOM times {}!!!", self.strength);
+    }
+}
 fn main() {
     let _x = HasDrop;
+
+    let _firecracker = Firework{strength: 100};
+    let _tnt = Firework{strength:1};
 } // x is out of scope here now, then drop() is called
