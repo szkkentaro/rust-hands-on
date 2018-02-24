e28ff12f8bec2927feb824250c5bb70d6f6889e8
Kentaro SUZUKI <kentarock@gmail.com>
2018-02-24 12:01:18 +0900

    feat: Add closure sample

---
+++ closures/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    // without return type
+    let plus_one = |x: i32| x + 1;
+    assert_eq!(2, plus_one(1));
+
+    // with return type
+    let plus_two = |x| -> i32 {
+        let mut result: i32 = x;
+        result += 1;
+        result += 1;
+        result
+    };
+    assert_eq!(3, plus_two(1));
+}
