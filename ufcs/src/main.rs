72c1a83410c9b7c12b2216c2f37f6a09bec22295
Kentaro SUZUKI <kentarock@gmail.com>
2018-02-26 10:06:59 +0900

    feat: Add angle-bracket form

--- ufcs/src/main.rs
+++ ufcs/src/main.rs
@@ -1,13 +1,25 @@
 trait Foo {
     fn f(&self);
+    fn num() -> i32;
 }
+
 trait Bar {
     fn f(&self);
 }
+
 struct Baz;
+impl Baz {
+    fn num() -> i32 {
+        20
+    }
+}
 impl Foo for Baz {
     fn f(&self) {
         println!("Foo for Baz");
+    }
+    
+    fn num() -> i32 {
+        10
     }
 }
 impl Bar for Baz {
@@ -20,4 +32,8 @@ fn main() {
     let b = Baz;
     Foo::f(&b);
     Bar::f(&b);
+
+    assert_eq!(10, <Baz as Foo>::num());
+    assert_eq!(20, Baz::num());
+    
 }
