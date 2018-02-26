8acaae175c35b62eb49213f4d64c292c714937e4
Kentaro SUZUKI <kentarock@gmail.com>
2018-02-26 09:57:00 +0900

    feat: Add universal function call syntax

---
+++ ufcs/src/main.rs
@@ -0,0 +1,23 @@
+trait Foo {
+    fn f(&self);
+}
+trait Bar {
+    fn f(&self);
+}
+struct Baz;
+impl Foo for Baz {
+    fn f(&self) {
+        println!("Foo for Baz");
+    }
+}
+impl Bar for Baz {
+    fn f(&self) {
+        println!("Bar for Baz");
+    }
+}
+
+fn main() {
+    let b = Baz;
+    Foo::f(&b);
+    Bar::f(&b);
+}
