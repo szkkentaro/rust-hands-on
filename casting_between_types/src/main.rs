f5952a92d6a27b71697269112284dfa90ce6134f
Kentaro SUZUKI <kentarock@gmail.com>
2018-03-01 09:29:04 +0900

    feat: Add transmute  sample

--- casting_between_types/src/main.rs
+++ casting_between_types/src/main.rs
@@ -1,3 +1,5 @@
+use std::mem;
+
 fn main() {
     // as is safe
     let x: i32 = 5;
@@ -14,6 +16,11 @@ fn main() {
     let b = a as u32;
     println!("{:?}, {}", a, b);
 
-    // transnute is danger
+    // transmute is unsafe
+    unsafe {
+        let c = [0u8, 0u8, 0u8, 1u8];
+        let d = mem::transmute::<[u8; 4], u32>(c);
+        println!("{:?}, {}", c, d);
+    }
 
 }
