b8bdb2f17c09ad8da34e3d16dd834769ff7d5e3e
Kentaro SUZUKI <kentarock@gmail.com>
2018-03-03 13:44:08 +0900

    feat: Using operator traits in generic structs

--- operators_and_overloading/src/main.rs
+++ operators_and_overloading/src/main.rs
@@ -1,4 +1,23 @@
 use std::ops::Add;
+use std::ops::Mul;
+
+trait HasArea<T> {
+    fn area(&self) -> T;
+}
+struct Square<T> {
+    x: T,
+    y: T,
+    side: T,
+}
+
+impl<T> HasArea<T> for Square<T>
+where
+    T: Mul<Output = T> + Copy,
+{
+    fn area(&self) -> T {
+        self.side * self.side
+    }
+}
 
 #[derive(Debug)]
 struct Point {
@@ -22,4 +41,11 @@ fn main() {
     let p2 = Point { x: 2, y: 3 };
     let p3 = p1 + p2;
     println!("{:?}", p3);
+
+    let s = Square {
+        x: 0.0f64,
+        y: 0.0f64,
+        side: 12.0f64,
+    };
+    println!("{}, {}, {}", s.x, s.y, s.area());
 }
