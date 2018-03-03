c6b995f0a16142f4f3b35ffb00095d9fc1b9e547
Kentaro SUZUKI <kentarock@gmail.com>
2018-03-04 06:20:17 +0900

    feat: Add hygiene at macros

--- macros/src/main.rs
+++ macros/src/main.rs
@@ -17,6 +17,14 @@ macro_rules! o_O {
         &[ $($($x + $y),*),* ]
     )
 }
+
+macro_rules! five_times {
+    (
+        $x:expr
+    ) => (
+        5 * $x
+    );
+}
 fn main() {
     foo!(x => 3);
     // foo!(y => 3); // compile error: No rules expected the token `y`
@@ -24,4 +32,6 @@ fn main() {
     let a: &[i32] = o_O!(10; [1,2,3];
                          20; [4,5,6]);
     assert_eq!(a, [11, 12, 13, 24, 25, 26]);
+
+    println!("{}", five_times!(2 + 3)); // It returns 25 as 5 * (2 + 3), not 13 as 5 * 2 + 3
 }
