3058024c281aa8a138aa3b726df98d10599dd599
Kentaro SUZUKI <kentarock@gmail.com>
2018-03-04 06:13:15 +0900

    feat: repetition at macros

--- macros/src/main.rs
+++ macros/src/main.rs
@@ -1,7 +1,27 @@
+// Matching
 macro_rules! foo {
-    (x => $e:expr) => (println!("mode X: {}", $e));
+    (
+        x => $e:expr
+    ) => (
+        println!("mode X: {}", $e)
+    );
+}
+
+// Repetition
+macro_rules! o_O {
+    (
+        $(
+            $x:expr; [ $($y:expr),* ]
+        );*
+    ) => (
+        &[ $($($x + $y),*),* ]
+    )
 }
 fn main() {
     foo!(x => 3);
     // foo!(y => 3); // compile error: No rules expected the token `y`
+
+    let a: &[i32] = o_O!(10; [1,2,3];
+                         20; [4,5,6]);
+    assert_eq!(a, [11, 12, 13, 24, 25, 26]);
 }
