18df94fe906ff72567e3e18f5e99e57775f6bd3a
Kentaro SUZUKI <kentarock@gmail.com>
2018-03-04 06:31:33 +0900

    feat: Add pass variant name to macro

--- macros/src/main.rs
+++ macros/src/main.rs
@@ -18,6 +18,7 @@ macro_rules! o_O {
     )
 }
 
+// Hygiene
 macro_rules! five_times {
     (
         $x:expr
@@ -25,13 +26,29 @@ macro_rules! five_times {
         5 * $x
     );
 }
+
+// pass variant name
+macro_rules! bar {
+    (
+        $v:ident
+    ) => (
+        let $v = 3;
+    );
+}
+
 fn main() {
     foo!(x => 3);
-    // foo!(y => 3); // compile error: No rules expected the token `y`
+    // compile error: No rules expected the token `y`
+    // foo!(y => 3);
 
     let a: &[i32] = o_O!(10; [1,2,3];
                          20; [4,5,6]);
     assert_eq!(a, [11, 12, 13, 24, 25, 26]);
 
-    println!("{}", five_times!(2 + 3)); // It returns 25 as 5 * (2 + 3), not 13 as 5 * 2 + 3
+    // It returns 25 as 5 * (2 + 3), not 13 as 5 * 2 + 3
+    println!("{}", five_times!(2 + 3));
+
+    // pass variant name to macro
+    bar!(x);
+    println!("{}", x); // print 3
 }
