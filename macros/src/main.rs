43169c174c4189416d9546927c8651e689b34d0f
Kentaro SUZUKI <kentarock@gmail.com>
2018-03-05 09:16:10 +0900

    feat: Add recursive macros

--- macros/src/main.rs
+++ macros/src/main.rs
@@ -36,6 +36,20 @@ macro_rules! bar {
     );
 }
 
+// requrcive macro
+macro_rules! write_html {
+    ($w:expr, ) => (());
+
+    ($w:expr, $e:tt) => (write!($w, "{}", $e));
+
+    ($w:expr, $tag:ident [ $($inner:tt)* ] $($rest:tt)*) => {{
+        write!($w, "<{}>", stringify!($tag));
+        write_html!($w, $($inner)*);
+        write!($w, "</{}>", stringify!($tag));
+        write_html!($w, $($rest)*);
+   }};
+}
+
 fn main() {
     foo!(x => 3);
     // compile error: No rules expected the token `y`
@@ -51,4 +65,19 @@ fn main() {
     // pass variant name to macro
     bar!(x);
     println!("{}", x); // print 3
+
+    use std::fmt::Write;
+    let mut out = String::new();
+
+    write_html!(&mut out,
+        html[
+            head[title["Macros guide"]]
+            body[h1["Macros are best!"]]
+        ]);
+
+    assert_eq!(
+        out,
+        "<html><head><title>Macros guide</title></head>\
+         <body><h1>Macros are best!</h1></body></html>"
+    );
 }
