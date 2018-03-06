108e254111610bd81d822bff8e49b8d6a0230e12
Kentaro SUZUKI <kentarock@gmail.com>
2018-03-06 09:03:45 +0900

    feat: Add ref and raw_pointers

--- raw_pointers/src/main.rs
+++ raw_pointers/src/main.rs
@@ -8,4 +8,16 @@ fn main() {
 
     println!("{}, {:?}, {}", x, raw, points_at);
     println!("{}, {:?}", y, raw_mut);
+
+    // 明示的
+    let i: u32 = 1;
+    let p_imm: *const u32 = &i as *const u32;
+    // 暗黙的
+    let mut m: u32 = 2;
+    let p_mut: *mut u32 = &mut m; // `as *const u32` is nothing
+    unsafe {
+        let ref_imm: &u32 = &*p_imm;
+        let ref_mut: &mut u32 = &mut *p_mut;
+        println!("{:?}, {:?}", ref_imm, ref_mut);
+    }
 }
