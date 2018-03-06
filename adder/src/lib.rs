c895c2b2c92882515264a37ee54c3e0c49accd64
Kentaro SUZUKI <kentarock@gmail.com>
2018-03-07 08:43:35 +0900

    feat: test attribute

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,9 +1,21 @@
+pub fn add_two(x: i32) -> i32 {
+    x + 2
+}
+
 #[cfg(test)]
 mod tests {
+    pub fn add_two(x: i32) -> i32 {
+        x + 2
+    }
+
     #[test]
-    #[should_panic]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
-        assert!(false);
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    #[should_panic(expected = "assertion failed")]
+    fn it_works_other() {
+        assert_eq!("Hello", "World");
     }
 }
