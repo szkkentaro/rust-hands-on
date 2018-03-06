e2efb17d5823f31b5283505211270712a6fcb7fa
Kentaro SUZUKI <kentarock@gmail.com>
2018-03-07 08:50:39 +0900

    feat: Add tests modules sample

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,13 +1,19 @@
-pub fn add_two(x: i32) -> i32 {
-    x + 2
+pub fn add_one(x: i32) -> i32 {
+    x + 1
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     pub fn add_two(x: i32) -> i32 {
         x + 2
     }
 
+    #[test]
+    fn pub_fn_works() {
+        assert_eq!(3, add_one(2));
+    }
     #[test]
     fn it_works() {
         assert_eq!(4, add_two(2));
