aab6aa3776b1418cc57ed22e217e19967a8f1cbb
Kentaro SUZUKI <kentarock@gmail.com>
2018-03-08 08:45:12 +0900

    feat: Add cfg macro sample

--- conditional_compilation/src/main.rs
+++ conditional_compilation/src/main.rs
@@ -1,4 +1,8 @@
 #[cfg(any(not(unix), all(target_os = "macos", target_arch = "x86_64")))]
 fn main() {
     println!("Hello, world!");
+
+    if cfg!(target_os = "macos") || cfg!(target_os = "ios") {
+        println!("Think Different!");
+    }
 }
