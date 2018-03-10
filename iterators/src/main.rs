89e4e01e1a09e21307b704d061e94c857e29d6cc
Kentaro SUZUKI <kentarock@gmail.com>
2018-03-10 09:20:31 +0900

    feat: Add iterators sample

---
+++ iterators/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let mut range = 0..10;
+    loop {
+        match range.next() {
+            Some(x) => {
+                println!("{}", x);
+            }
+            None => break,
+        }
+    }
+}
