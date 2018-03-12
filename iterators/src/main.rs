11002f10a0598868255733ca2ae0e5c2b49bfed0
Kentaro SUZUKI <kentarock@gmail.com>
2018-03-12 09:17:04 +0900

    feat: Add find consumer sample

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -24,4 +24,12 @@ fn main() {
 
     // consumer with type placeholder
     let one_to_one_hundred = (1..101).collect::<Vec<_>>();
+
+    // find consumer
+    let greater_than_forty_two = (0..100).find(|x| *x > 42);
+
+    match greater_than_forty_two {
+        Some(_) => println!("Found a match!"),
+        None => println!("No match found"),
+    }
 }
