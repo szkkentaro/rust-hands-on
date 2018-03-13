0b33e396775ac3093fd2d3150a8520575fe56822
Kentaro SUZUKI <kentarock@gmail.com>
2018-03-14 08:47:57 +0900

    feat: Add channel sample

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -1,6 +1,7 @@
+use std::time::Duration;
 use std::thread;
 use std::sync::{Arc, Mutex};
-use std::time::Duration;
+use std::sync::mpsc;
 
 fn main() {
     let data = Arc::new(Mutex::new(vec![1, 2, 3]));
@@ -13,4 +14,31 @@ fn main() {
         });
     }
     thread::sleep(Duration::from_millis(50));
+
+    let data = Arc::new(Mutex::new(0));
+    let (tx, rx) = mpsc::channel();
+    for _ in 0..10 {
+        let (data, tx) = (data.clone(), tx.clone());
+        thread::spawn(move || {
+            let mut data = data.lock().unwrap();
+            *data += 1;
+            tx.send(()).unwrap();
+        });
+    }
+    for _ in 0..10 {
+        rx.recv().unwrap();
+    }
+
+    let (tx, rx) = mpsc::channel();
+    for i in 0..10 {
+        let tx = tx.clone();
+        thread::spawn(move || {
+            let answer = i * i;
+
+            tx.send(answer).unwrap();
+        });
+    }
+    for _ in 0..10 {
+        println!("{:?}", rx.recv().unwrap());
+    }
 }
