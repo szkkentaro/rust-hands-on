558ef3e3837ec90c018ee286c2f8c96201bf827b
Kentaro SUZUKI <kentarock@gmail.com>
2018-03-15 08:48:09 +0900

    feat: Add match is into unwrap function sample

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,10 +1,36 @@
+#[allow(unused_imports)]
+use std::env;
+
+#[allow(unused_variables, dead_code)]
 fn guess(i: i32) -> bool {
     if i < 0 || i > 10 {
         panic!("Invalid number: {}", i);
     }
     i == 5
 }
+
+fn find(haystack: &str, needle: char) -> Option<usize> {
+    for (offset, c) in haystack.char_indices() {
+        if needle == c {
+            return Some(offset);
+        }
+    }
+    None
+}
+
 fn main() {
     // The Basics
-    guess(11);
+    // guess(11);
+
+    // The Basics 2
+    // let mut arg = env::args();
+    // let arg: String = arg.nth(1).unwrap(); // error 1
+    // let n: i32 = arg.parse().unwrap(); // error 2
+    // println!("{}", n * 2);
+
+    let file_name = "foobar.rs";
+    match find(file_name, '.') {
+        None => println!("No file extension found."),
+        Some(i) => println!("File extension: {}", &file_name[i + 1..]),
+    }
 }
