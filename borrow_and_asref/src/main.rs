edcbf5ae8338db50a36e50cb9982a42e08172211
Kentaro SUZUKI <kentarock@gmail.com>
2018-03-29 09:25:44 +0900

    feat: Add borrow_and_asref sample

---
+++ borrow_and_asref/src/main.rs
@@ -0,0 +1,17 @@
+// use std::collections::HashMap;
+use std::borrow::Borrow;
+use std::fmt::Display;
+
+fn main() {
+    fn foo<T: Borrow<i32> + Display>(a: T) {
+        println!("a is borrowd: {}", a);
+    }
+
+    let mut i = 5;
+    foo(&i);
+    foo(&mut i);
+
+    // let mut map = HashMap::new();
+    // map.insert("Foo".to_string(), 42);
+    // assert_eq!(map.get("Foo"), Some(&42));
+}
