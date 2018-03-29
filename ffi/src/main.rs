2b566707ecb9bffc096ae045193725615a6dcc7b
Kentaro SUZUKI <kentarock@gmail.com>
2018-03-29 09:18:03 +0900

    refactor: call c lib

--- ffi/src/main.rs
+++ ffi/src/main.rs
@@ -1,12 +1,26 @@
 extern crate libc;
-use libc::size_t;
+// use libc::size_t;
 
-#[link(name = "snappy")]
-extern "C" {
-    fn snappy_max_compressed_length(source_length: size_t) -> size_t;
+extern "C" fn callback(a: i32) {
+    println!("callback from {0}", a);
 }
 
+#[link(name = "extlib", kind = "static")]
+extern "C" {
+    fn register_callback(cb: extern "C" fn(i32)) -> i32;
+    fn trigger_callback();
+}
+
+// #[link(name = "snappy")]
+// extern "C" {
+//     fn snappy_max_compressed_length(source_length: size_t) -> size_t;
+// }
+
 fn main() {
-    let x = unsafe { snappy_max_compressed_length(100) };
-    println!("max compressed length of a 100 byte buffer: {}", x);
+    // let x = unsafe { snappy_max_compressed_length(100) };
+    // println!("max compressed length of a 100 byte buffer: {}", x);
+    unsafe {
+        register_callback(callback);
+        trigger_callback();
+    }
 }
