e28f4c4471341d9dd890f394527d8706d82481d8
Kentaro SUZUKI <kentarock@gmail.com>
2018-03-29 09:19:25 +0900

    fix: remove kind

--- ffi/src/main.rs
+++ ffi/src/main.rs
@@ -5,7 +5,7 @@ extern "C" fn callback(a: i32) {
     println!("callback from {0}", a);
 }
 
-#[link(name = "extlib", kind = "static")]
+#[link(name = "extlib")]
 extern "C" {
     fn register_callback(cb: extern "C" fn(i32)) -> i32;
     fn trigger_callback();
