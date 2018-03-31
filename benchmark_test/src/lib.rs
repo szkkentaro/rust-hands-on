fa2ed854bc7d602a7e869a6055510828b7ebe0c7
Kentaro SUZUKI <kentarock@gmail.com>
2018-04-01 03:28:44 +0900

    feat: Add how to avoid optimizer

--- benchmark_test/src/lib.rs
+++ benchmark_test/src/lib.rs
@@ -20,4 +20,9 @@ mod tests {
     fn bench_add_two(b: &mut Bencher) {
         b.iter(|| add_two(2));
     }
+
+    #[bench]
+    fn bench_xor_1000_ints(b: &mut Bencher) {
+        b.iter(|| (0..1000).fold(0, |old, new| old ^ new));
+    }
 }
