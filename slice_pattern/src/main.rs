95a9eb084fa8dfc0331647a43e5d913d2328f145
Kentaro SUZUKI <kentarock@gmail.com>
2018-04-05 09:01:38 +0900

    feat: Add symmertic sample

--- slice_pattern/src/main.rs
+++ slice_pattern/src/main.rs
@@ -1,4 +1,12 @@
-#![feature(slice_pattern)]
+#![feature(slice_patterns)]
+
+fn is_symmetric(list: &[u32]) -> bool {
+    match list {
+        [] | [_] => true,
+        [x, inside.., y] if x == y => is_symmetric(inside),
+        _ => false,
+    }
+}
 
 fn main() {
     let v = vec!["match_this", "1"];
@@ -6,4 +14,7 @@ fn main() {
         ["match_this", second] => println!("This is second value: {}", second),
         _ => {},
     }
+
+    let sym = &[0,1,2,4,2,1,0];
+    assert!(is_symmetric(sym));
 }
