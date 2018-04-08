51dfddb424226e3a31035ae62b45b3cbff57394c
Kentaro SUZUKI <suzukik@myMBP.local>
2018-04-08 20:35:29 +0900

    refactor: Add another type associated const sample

--- associated_consts/src/main.rs
+++ associated_consts/src/main.rs
@@ -6,6 +6,10 @@ impl Foo for i32 {
     const ID: i32 = 1;
 }
 
+impl Foo for i64 {
+    const ID: i32 = 5;
+}
+
 struct Bar;
 
 impl Bar {
@@ -14,5 +18,6 @@ impl Bar {
 
 fn main() {
     assert_eq!(1, i32::ID);
+    assert_eq!(5, i64::ID);
     println!("{}", Bar::ID);
 }
